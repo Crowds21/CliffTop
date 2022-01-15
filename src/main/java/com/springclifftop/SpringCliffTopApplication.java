a8d751f68eff005ddafc7e6331b0e9f23f711fae
crowds <717948932@qq.com>
2022-01-15 10:27:10 +0800

    Update

--- src/main/java/com/springclifftop/SpringCliffTopApplication.java
+++ src/main/java/com/springclifftop/SpringCliffTopApplication.java
@@ -1,9 +1,7 @@
 package com.springclifftop;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
